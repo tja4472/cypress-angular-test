1b7537b2f60afbb0ad69e6c361683f74684d37e6
Tim <tja2353@gmail.com>
2022-04-09 11:39:53 +0100

    Add @jscutlery/cypress-angular

---
+++ projects/my-project/cypress/plugins/index.ts
@@ -0,0 +1,12 @@
+import { startAngularDevServer } from '@jscutlery/cypress-angular';
+
+module.exports = (on, config) => {
+  on('dev-server:start', (options) =>
+    startAngularDevServer({
+      options,
+      tsConfig: 'tsconfig.cypress.json',
+      target: 'my-project:build',
+    })
+  );
+  return config;
+};
